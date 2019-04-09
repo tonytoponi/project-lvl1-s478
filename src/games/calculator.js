77e4b50ba830f5fbbdc204c59b01d16cb940f4ff
Anton Zhilkin <tonyzhilkin@gmail.com>
2019-04-10 02:41:41 +0300

    Refactor calculator logic

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -1,55 +1,23 @@
+import { car, cdr } from 'hexlet-pairs';
 import showSuccessMessage from '../showSuccessMessage';
 import showFailMessage from '../showFailMessage';
 import getRandomNumber from '../getRandomNumber';
 import showQuestion from '../showQuestion';
+import returnRandomOperator from '../returnRandomOperator';
 
 export default (name) => {
   const value1 = getRandomNumber(0, 10);
   const value2 = getRandomNumber(0, 10);
-  const sign = getRandomNumber(1, 4);
-  if (sign === 1) {
-    const questonText = `${value1} + ${value2}`;
-    const answer = parseInt(showQuestion(questonText), 10);
-    const correctAnswer = value1 + value2;
-    if (answer === correctAnswer) {
-      showSuccessMessage();
-      return true;
-    }
-    showFailMessage(answer, correctAnswer, name);
-    return false;
+  const operator = returnRandomOperator();
+  const calculate = car(operator);
+  const operatorSign = cdr(operator);
+  const questonText = `${value1} ${operatorSign} ${value2}`;
+  const answer = parseInt(showQuestion(questonText), 10);
+  const correctAnswer = calculate(value1, value2);
+  if (answer === correctAnswer) {
+    showSuccessMessage();
+    return true;
   }
-  if (sign === 2) {
-    const questonText = `${value1} - ${value2}`;
-    const answer = parseInt(showQuestion(questonText), 10);
-    const correctAnswer = value1 - value2;
-    if (answer === correctAnswer) {
-      showSuccessMessage();
-      return true;
-    }
-    showFailMessage(answer, correctAnswer, name);
-    return false;
-  }
-  if (sign === 3) {
-    const questonText = `${value1} * ${value2}`;
-    const answer = parseInt(showQuestion(questonText), 10);
-    const correctAnswer = value1 * value2;
-    if (answer === correctAnswer) {
-      showSuccessMessage();
-      return true;
-    }
-    showFailMessage(answer, correctAnswer, name);
-    return false;
-  }
-  if (sign === 4) {
-    const questonText = `${value1} / ${value2}`;
-    const answer = parseInt(showQuestion(questonText), 10);
-    const correctAnswer = value1 / value2;
-    if (answer === correctAnswer) {
-      showSuccessMessage();
-      return true;
-    }
-    showFailMessage(answer, correctAnswer, name);
-    return false;
-  }
-  return 0;
+  showFailMessage(answer, correctAnswer, name);
+  return false;
 };
