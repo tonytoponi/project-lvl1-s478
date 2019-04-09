60bd911f52f100eabe40807a728ee653337ead93
Anton Zhilkin <tonyzhilkin@gmail.com>
2019-04-10 02:59:41 +0300

    Fix naming

--- src/returnRandomOperator.js
+++ src/returnRandomOperator.js
@@ -5,14 +5,14 @@ export default () => {
   const sign = getRandomNumber(1, 4);
   switch (sign) {
     case 1:
-      return () => cons((a, b) => a + b, '/');
+      return () => cons((a, b) => a + b, '+');
     case 2:
-      return () => cons((a, b) => a - b, '/');
+      return () => cons((a, b) => a - b, '-');
     case 3:
-      return () => cons((a, b) => a * b, '/');
+      return () => cons((a, b) => a * b, '*');
     case 4:
       return () => cons((a, b) => a / b, '/');
     default:
-      return (a, b) => a + b;
+      return 0;
   }
 };
