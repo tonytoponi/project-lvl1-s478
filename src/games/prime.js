da7113d74273262097bb3656e269e1651669e162
Anton Zhilkin <tonyzhilkin@gmail.com>
2019-04-12 02:15:12 +0300

    Prime game logic

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import { cons } from 'hexlet-pairs';
+import gameEngine from '../gameEngine';
+import getRandomNumber from '../getRandomNumber';
+
+const isPrime = (num) => {
+  const iterator = (acc) => {
+    if (num % acc === 0) {
+      return false;
+    }
+    if (acc > Math.sqrt(num)) {
+      return true;
+    }
+    return iterator(acc + 1);
+  };
+  return iterator(2);
+};
+
+const getPrimeGameData = () => {
+  const question = getRandomNumber(0, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  const gameData = cons(question, correctAnswer);
+  return gameData;
+};
+
+export default () => {
+  const evenGameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  gameEngine(getPrimeGameData, evenGameRules);
+};
